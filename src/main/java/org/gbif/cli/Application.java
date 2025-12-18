a762555b7d2b0929dec1b5c2ef2683be400d2cc2
fmendezh <federicomh@gmail.com>
2025-12-18 14:51:45 +0100

    rolling back changes to application, it is better to assume that logback is the classpath

--- src/main/java/org/gbif/cli/Application.java
+++ src/main/java/org/gbif/cli/Application.java
@@ -133,16 +133,10 @@ public class Application {
    * The drawback here is that it doesn't take into account the configurability of logback files using the BaseCommand.
    */
   private void configureLogback() {
-    org.slf4j.ILoggerFactory ilf = LoggerFactory.getILoggerFactory();
-    if (ilf instanceof LoggerContext) {
-      LoggerContext context = (LoggerContext) ilf;
-      URL loggingConfigUrl = ConfigurationWatchListUtil.getMainWatchURL(context);
-      if (loggingConfigUrl == null) {
-        context.reset();
-      }
-    } else {
-      LOG.warn("No logback LoggerContext available (found {}). SLF4J is using a different binding; logging configuration will be skipped.",
-          ilf.getClass().getName());
+    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    URL loggingConfigUrl = ConfigurationWatchListUtil.getMainWatchURL(context);
+    if (loggingConfigUrl == null) {
+      context.reset();
     }
   }
 
